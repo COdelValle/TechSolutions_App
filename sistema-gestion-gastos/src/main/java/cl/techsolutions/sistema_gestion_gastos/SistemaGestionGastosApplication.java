92e3d056afe48848a8f387512fd2b74bbef3f248
Catalina Ormeño <208706730+COdelValle@users.noreply.github.com>
2025-06-09 20:07:10 -0400

    Creación_proyecto_con_dependencias
    Dependencias del proyecto:
    - Lombok.
    - Spring Web.
    - Spring Data JPA.
    - MySQL Driver.
    - Spring Security.
    - Validation.

---
+++ sistema-gestion-gastos/src/main/java/cl/techsolutions/sistema_gestion_gastos/SistemaGestionGastosApplication.java
@@ -0,0 +1,13 @@
+package cl.techsolutions.sistema_gestion_gastos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestionGastosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGestionGastosApplication.class, args);
+	}
+
+}
---
+++ sistema-gestion-gastos/src/test/java/cl/techsolutions/sistema_gestion_gastos/SistemaGestionGastosApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.techsolutions.sistema_gestion_gastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGestionGastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
